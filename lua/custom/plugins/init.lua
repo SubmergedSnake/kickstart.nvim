ea31fbddd347d7d9badb1afcaa73f381203c8fed
SubmergedSnake <solarshadow.jh@gmail.com>
2024-12-02 13:45:46 +0200

    nvimtree toggle

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,27 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+--
+
+-- Nvim tree
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- optionally enable 24-bit colour
+vim.opt.termguicolors = true
+-- custom mappings
+vim.keymap.set('n', '<C-n>', ':NvimTreeToggle<CR>')
+
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {}
+  end,
+}
