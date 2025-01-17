f18a20934a0112a7a74ce1de497b59679de41320
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-01-17 10:54:47 +0200

    Added Aerial plugin for file overview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -31,7 +31,28 @@ return {
       require('nvim-tree').setup {}
     end,
   },
-
+  -- AERIAL
+  {
+    'stevearc/aerial.nvim',
+    opts = {},
+    -- Optional dependencies
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('aerial').setup {
+        -- optionally use on_attach to set keymaps when aerial has attached to a buffer
+        on_attach = function(bufnr)
+          -- Jump forwards/backwards with '{' and '}'
+          vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+          vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
+        end,
+      }
+      -- You probably also want to set a keymap to toggle aerial
+      vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle!<CR>')
+    end,
+  },
   -- SURROUND
   {
     'tpope/vim-surround',
@@ -44,7 +65,6 @@ return {
   {
     'tpope/vim-repeat',
   },
-
   --GIT
   { 'tpope/vim-fugitive' },
 
