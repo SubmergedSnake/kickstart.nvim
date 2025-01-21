ba70472b9379c3e4c206eda7f981ff4aa21b0912
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-01-21 14:44:29 +0200

    Dim inactive windows in nvim everforest

--- init.lua
+++ init.lua
@@ -847,6 +847,11 @@ require('lazy').setup({
     --'folke/tokyonight.nvim',
     'neanias/everforest-nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    config = function()
+      require('everforest').setup {
+        dim_inactive_windows = true,
+      }
+    end,
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
