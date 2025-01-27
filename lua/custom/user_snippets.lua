346ec43e5918e8134e7ac093f3ddace76a5cade5
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-01-27 09:05:24 +0200

    include previous snippets for tsx, too

--- lua/custom/user_snippets.lua
+++ lua/custom/user_snippets.lua
@@ -4,7 +4,7 @@ local s = ls.snippet
 local t = ls.text_node
 local i = ls.insert_node
 
-local filetypes = { 'javascript', 'typescript' }
+local filetypes = { 'javascript', 'typescript', 'typescriptreact' }
 
 for _, ft in ipairs(filetypes) do
   ls.add_snippets(ft, {
