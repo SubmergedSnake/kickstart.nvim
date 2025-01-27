3792995df5454fb0f3c7fb13b2dc2b8dbda25933
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-01-27 16:10:23 +0200

    New plugin and snippets
    Adds new plugin, boole, to toggle booleans and yes/no etc.
    Also adds custom snippets for creating describe, test and expect

--- init.lua
+++ init.lua
@@ -977,7 +977,9 @@ require('lazy').setup({
 })
 
 -- Custom snippets
-require 'custom.user_snippets'
+
+-- JS
+require 'custom.snippets.js_snippets'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/snippets/js_snippets.lua
+++ lua/custom/snippets/js_snippets.lua
@@ -8,7 +8,7 @@ local filetypes = { 'javascript', 'typescript', 'typescriptreact' }
 
 for _, ft in ipairs(filetypes) do
   ls.add_snippets(ft, {
-    s('td', {
+    s('_desc', {
       t "describe('",
       i(1, 'description'),
       t { "', () => {", '\t' },
@@ -18,7 +18,7 @@ for _, ft in ipairs(filetypes) do
   })
 
   ls.add_snippets(ft, {
-    s('tt', {
+    s('_t', {
       t "it('",
       i(1, 'it should'),
       t { "', () => {", '\t' },
@@ -27,8 +27,19 @@ for _, ft in ipairs(filetypes) do
     }),
   })
 
+  -- async variant of test()
   ls.add_snippets(ft, {
-    s('tx', {
+    s('_ta', {
+      t "it('",
+      i(1, 'it should'),
+      t { "', async () => {", '\t' },
+      i(2, '// test implementation'),
+      t { '', '})' },
+    }),
+  })
+
+  ls.add_snippets(ft, {
+    s('_ex', {
       t 'expect(',
       i(1, 'target'),
       t { ').' },
