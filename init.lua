126d3dfc1f11cd96bfb0f4f38f5d11a665bee98b
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-02-06 10:42:58 +0200

    Added groovy ls

--- init.lua
+++ init.lua
@@ -615,6 +615,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        groovyls = { cmd = { '/home/joonas/.local/share/nvim/mason/bin/groovy-language-server' } },
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
