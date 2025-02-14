68d39dda61a0da1ecb33f47edc8d59ee7f314738
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-02-14 13:30:26 +0200

    clear jumplist upon entering Neovim

--- init.lua
+++ init.lua
@@ -204,6 +204,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Clear jumplist when starting Neovim
+vim.api.nvim_create_autocmd('VimEnter', {
+  pattern = '*',
+  callback = function()
+    vim.cmd 'clearjumps'
+  end,
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
